452ae9fc5be9a8a00b6cc87d0d4fa580e1fca3a0
phanideep <40293400+vphanideep@users.noreply.github.com>
2023-03-14 19:11:32 +0530

    Create testt.groovy

---
+++ testt.groovy
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+              script {
+                cat  ${workspace}/current.txt | tr [:lower:] [:upper:]").trim() + ".zip" 
+              }
+            }
+        }
+    }
+}
